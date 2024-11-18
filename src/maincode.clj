264b735e30054d9104cf7761252d47b693e0e44f
andjela-mircetic <andjelamircetic55@gmail.com>
2024-11-18 14:48:38 +0100

    use cases for showing messages

--- src/maincode.clj
+++ src/maincode.clj
@@ -2,13 +2,24 @@
   (:require [nrepl.server :as nrepl-server]
             [cider.nrepl :refer (cider-nrepl-handler)]))
 
+(def messages
+  [{:sentFrom "Andjela" :sentTo "Katarina" :time "2024-11-18 12:00" :message "Hello!"}
+   {:sentFrom "Katarina" :sentTo "Andjela" :time "2024-11-18 12:05" :message "Hi Angie!"}])
+
+(def users #{"Andjela" "Katarina"})
+
 (defn register-user [users username]
   (if (contains? users username)
     {:error "Username already exists"}
-    (assoc users username [])))
+    (conj users username)))
 
-(def users {})
-(def users (register-user users "Andjela"))
-(def users (register-user users "Katarina"))
+(defn getAllMessagesByUser [messages username]
+  (filter #(or (= (:sentFrom %) username)
+               (= (:sentTo %) username))
+          messages))
 
+(defn messagesSentBy [messages username]
+  (filter #(= (:sentFrom %) username) messages))
 
+(defn messagesReceivedBy [messages username]
+  (filter #(= (:sentTo %) username) messages))
--- test/maincode_test.clj
+++ test/maincode_test.clj
@@ -2,18 +2,42 @@
   (:require [midje.sweet :refer :all]
             [maincode :refer :all]))
 
-(fact "registers a new user"
-      (register-user {} "Andjela") => {"Andjela" []})
+(fact "register a new user"
+      (register-user #{} "Andjela") => #{"Andjela"})
 
-(fact "registers multiple users"
-      (-> {}
+(fact "register multiple users"
+      (-> #{}
           (register-user "Andjela")
-          (register-user "Katarina")) => {"Andjela" [] "Katarina" []})
+          (register-user "Katarina")) => #{"Andjela" "Katarina"})
 
-(fact "does not allow duplicate usernames"
-      (register-user {"Andjela" []} "Andjela") => {:error "Username already exists"})
+(fact "test if duplicate usernames are allowed"
+      (register-user #{"Andjela"} "Andjela") => {:error "Username already exists"})
 
-(fact "ensures existing users are unchanged"
-      (register-user {"Andjela" [] "Katarina" []} "Dusan")
-      => {"Andjela" [] "Katarina" [] "Dusan" []})
+(fact "test if existing users are unchanged"
+      (register-user #{"Andjela" "Katarina"} "Dusan")
+      => #{"Andjela" "Katarina" "Dusan"})
+
+(ns maincode-test
+  (:require [midje.sweet :refer :all]
+            [maincode :refer :all]))
+
+(fact "returns all messages sent and received by a user"
+      (getAllMessagesByUser
+       [{:sentFrom "Andjela" :sentTo "Katarina" :time "2024-11-18 12:00" :message "Hi!"}
+        {:sentFrom "Katarina" :sentTo "Andjela" :time "2024-11-18 12:05" :message "Hello!"}
+        {:sentFrom "Dusan" :sentTo "Andjela" :time "2024-11-18 12:10" :message "Good day!"}]
+       "Andjela")
+      => [{:sentFrom "Andjela" :sentTo "Katarina" :time "2024-11-18 12:00" :message "Hi!"}
+          {:sentFrom "Katarina" :sentTo "Andjela" :time "2024-11-18 12:05" :message "Hello!"}
+          {:sentFrom "Dusan" :sentTo "Andjela" :time "2024-11-18 12:10" :message "Good day!"}])
+
+(fact "returns an empty list if the user has no messages"
+      (getAllMessagesByUser
+       [{:sentFrom "Andjela" :sentTo "Katarina" :time "2024-11-18 12:00" :message "Hi!"}
+        {:sentFrom "Katarina" :sentTo "Andjela" :time "2024-11-18 12:05" :message "Hello!"}]
+       "Dusan")
+      => ())
+
+
+
 
